552bc856d28c8ed8c6e0f789638bb4d5cfee4985
Tharindu <ku.tharindu@gmail.com>
2022-03-15 09:08:44 +0530

    service connection created

--- src/utils/enum.tsx
+++ src/utils/enum.tsx
@@ -1,5 +1,13 @@
 export const NAVIGATION_PAGES = {
-    Home: 'Home',
-    TradeBook: 'TradeBook',
-    Trades: 'Trades',
-}
+  Home: 'Home',
+  TradeBook: 'TradeBook',
+  Trades: 'Trades',
+};
+
+export const TRADE_BOOK_URL = 'wss://api-pub.bitfinex.com/ws/2'
+
+export const TRADE_BOOK_REQ = {
+  event: 'subscribe',
+  channel: 'book',
+  symbol: 'tBTCUSD',
+};
--- src/views/TradeBook.tsx
+++ src/views/TradeBook.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
+import TradeBookSocket from '../services/TradeBookSocket';
 
 const TradeBook = () => {
+
+  useEffect(() => {
+    TradeBookSocket().connect()
+
+    return () => {
+      TradeBookSocket().disconnect()
+    };
+  }, []);
+
   return <SafeAreaView></SafeAreaView>;
 };
-
+data: '[1942,[38817,2,0.28829127]]';
 export default TradeBook;
